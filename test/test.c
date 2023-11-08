5b0a4739ec5e4e7750053f43da460a7859f776d2
Piotr Szulakowski <pszulakowski321@gmail.com>
2023-11-08 01:06:14 +0100

    add assert_false assert_greater and assert_lower

--- test/test.c
+++ test/test.c
@@ -3,14 +3,17 @@
 NTestRunner testRunner;
 
 void test_example(int a, int b) {
-    n_test_assert_true(testRunner, a == b);
+    n_test_assert_true(testRunner, !b);
+    n_test_assert_false(testRunner, !b);
     n_test_assert_int32_equal(testRunner, a, b);
+    n_test_assert_int32_greater(testRunner, a, 1);
+    n_test_assert_int32_lower(testRunner, a, -1);
 }
 
 int main() {
     NLogger logger = n_logger_create(LOGGERMODE_CONSOLE, NULL);
     testRunner = n_test_runner_create(logger);
-    test_example(2, 4);
+    test_example(2, 0);
     test_example(-2, 4);
     test_example(3, 3);
     n_test_runner_destroy(testRunner);
