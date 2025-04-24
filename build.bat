1d437421b5217c7e801c0e3b19f6e65a5affcbb7
kkard2 <contact@kkard2.com>
2025-04-24 18:32:30 +0200

    this doesn't work

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ rem NOTE(kkard2): do not use setlocal, variables are used by test.bat
 rem TODO(kkard2): maybe we should have libs in a variable? currently repeating it twice
 
 rem Config
+set "CC=gcc"
 set SRC=src
 set BUILD=build
 set OBJ=obj
@@ -10,24 +11,32 @@ set TEST=test
 set OBJTEST=%OBJ%\test
 rem _Config
 
-set "C_FLAGS=/W3 /I%VULKAN_SDK%\Include /Iinclude /nologo /c"
+set "C_FLAGS=-Wall -I%VULKAN_SDK%\Include -Iinclude -c"
 mkdir %OBJ% 2> NUL
 
-cl %C_FLAGS% %SRC%\n_debug.c /Fo%OBJ%\n_debug.obj
-cl %C_FLAGS% %SRC%\n_error.c /Fo%OBJ%\n_error.obj
-cl %C_FLAGS% %SRC%\n_input_windows.c /Fo%OBJ%\n_input_windows.obj
-cl %C_FLAGS% %SRC%\n_math.c /Fo%OBJ%\n_math.obj
-cl %C_FLAGS% %SRC%\n_memory.c /Fo%OBJ%\n_memory.obj
-cl %C_FLAGS% %SRC%\n_string.c /Fo%OBJ%\n_string.obj
-cl %C_FLAGS% %SRC%\n_test.c /Fo%OBJ%\n_test.obj
+@echo on
+%CC% %C_FLAGS% %SRC%\n_debug.c -o %OBJ%\n_debug.o
+%CC% %C_FLAGS% %SRC%\n_error.c -o %OBJ%\n_error.o
+%CC% %C_FLAGS% %SRC%\n_input_windows.c -o %OBJ%\n_input_windows.o
+%CC% %C_FLAGS% %SRC%\n_math.c -o %OBJ%\n_math.o
+%CC% %C_FLAGS% %SRC%\n_memory.c -o %OBJ%\n_memory.o
+%CC% %C_FLAGS% %SRC%\n_string.c -o %OBJ%\n_string.o
+%CC% %C_FLAGS% %SRC%\n_test.c -o %OBJ%\n_test.o
 
-cl %C_FLAGS% %SRC%\graphics\lodepng.c /Fo%OBJ%\lodepng.obj
-cl %C_FLAGS% %SRC%\graphics\n_graphics_rasterizer.c /Fo%OBJ%\n_graphics_rasterizer.obj
-cl %C_FLAGS% %SRC%\graphics\n_graphics_vulkan.c /Fo%OBJ%\n_graphics_vulkan.obj
-cl %C_FLAGS% %SRC%\graphics\n_graphics_window_windows.c /Fo%OBJ%\n_graphics_window_windows.obj
+%CC% %C_FLAGS% %SRC%\graphics\lodepng.c -o %OBJ%\lodepng.o
+%CC% %C_FLAGS% %SRC%\graphics\n_graphics_rasterizer.c -o %OBJ%\n_graphics_rasterizer.o
+%CC% %C_FLAGS% %SRC%\graphics\n_graphics_vulkan.c -o %OBJ%\n_graphics_vulkan.o
+%CC% %C_FLAGS% %SRC%\graphics\n_graphics_window_windows.c -o %OBJ%\n_graphics_window_windows.o
+@echo off
 
 mkdir %BUILD% 2> NUL
-link /NOLOGO /DLL User32.lib Comctl32.lib %VULKAN_SDK%\Lib\vulkan-1.lib %OBJ%\*.obj ^
-    /OUT:%BUILD%\nandi.dll /IMPLIB:%BUILD%\nandi.lib && ^
-lib /NOLOGO %OBJ%\*.obj /OUT:%BUILD%\nandi.lib && ^
+
+set "ALL_OBJS="
+for %%f in (%OBJ%\*.o) do (
+    set "ALL_OBJS=!ALL_OBJS! %%f"
+)
+
+@echo on
+%CC% -o %BUILD%\nandi.dll %VULKAN_SDK%\Lib\vulkan-1.lib -lcomctl32 -lscrnsave %ALL_OBJ%
+@echo off
 echo ---------- built nandi.dll and nandi.lib ----------
