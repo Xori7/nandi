2b8ee6acbaeb150e413ba7b8a9395eb1afccd534
kkard2 <contact@kkard2.com>
2025-04-24 20:36:27 +0200

    works now

--- build.bat
+++ build.bat
@@ -1,9 +1,8 @@
 @echo off
 rem NOTE(kkard2): do not use setlocal, variables are used by test.bat
-rem TODO(kkard2): maybe we should have libs in a variable? currently repeating it twice
 
 rem Config
-set "CC=gcc"
+set "CC=zig cc"
 set SRC=src
 set BUILD=build
 set OBJ=obj
@@ -11,32 +10,36 @@ set TEST=test
 set OBJTEST=%OBJ%\test
 rem _Config
 
-set "C_FLAGS=-Wall -I%VULKAN_SDK%\Include -Iinclude -c"
+set "C_FLAGS=-g -Wall -std=c11"
+set "INCLUDES=-I%VULKAN_SDK%\Include -Iinclude"
+set "LIBS=-L%VULKAN_SDK%\Lib -lvulkan-1 -lcomctl32"
+
 mkdir %OBJ% 2> NUL
 
 @echo on
-%CC% %C_FLAGS% %SRC%\n_debug.c -o %OBJ%\n_debug.o
-%CC% %C_FLAGS% %SRC%\n_error.c -o %OBJ%\n_error.o
-%CC% %C_FLAGS% %SRC%\n_input_windows.c -o %OBJ%\n_input_windows.o
-%CC% %C_FLAGS% %SRC%\n_math.c -o %OBJ%\n_math.o
-%CC% %C_FLAGS% %SRC%\n_memory.c -o %OBJ%\n_memory.o
-%CC% %C_FLAGS% %SRC%\n_string.c -o %OBJ%\n_string.o
-%CC% %C_FLAGS% %SRC%\n_test.c -o %OBJ%\n_test.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_debug.c -o %OBJ%\n_debug.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_error.c -o %OBJ%\n_error.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_input_windows.c -o %OBJ%\n_input_windows.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_math.c -o %OBJ%\n_math.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_memory.c -o %OBJ%\n_memory.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_string.c -o %OBJ%\n_string.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\n_test.c -o %OBJ%\n_test.o
 
-%CC% %C_FLAGS% %SRC%\graphics\lodepng.c -o %OBJ%\lodepng.o
-%CC% %C_FLAGS% %SRC%\graphics\n_graphics_rasterizer.c -o %OBJ%\n_graphics_rasterizer.o
-%CC% %C_FLAGS% %SRC%\graphics\n_graphics_vulkan.c -o %OBJ%\n_graphics_vulkan.o
-%CC% %C_FLAGS% %SRC%\graphics\n_graphics_window_windows.c -o %OBJ%\n_graphics_window_windows.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\graphics\lodepng.c -o %OBJ%\lodepng.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\graphics\n_graphics_rasterizer.c -o %OBJ%\n_graphics_rasterizer.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\graphics\n_graphics_vulkan.c -o %OBJ%\n_graphics_vulkan.o
+%CC% -c %C_FLAGS% %INCLUDES% %SRC%\graphics\n_graphics_window_windows.c -o %OBJ%\n_graphics_window_windows.o
 @echo off
 
 mkdir %BUILD% 2> NUL
 
+setlocal enabledelayedexpansion
 set "ALL_OBJS="
 for %%f in (%OBJ%\*.o) do (
     set "ALL_OBJS=!ALL_OBJS! %%f"
 )
 
 @echo on
-%CC% -o %BUILD%\nandi.dll %VULKAN_SDK%\Lib\vulkan-1.lib -lcomctl32 -lscrnsave %ALL_OBJ%
+%CC% --shared -o %BUILD%\nandi.dll %ALL_OBJS% %LIBS%
 @echo off
-echo ---------- built nandi.dll and nandi.lib ----------
+echo ---------- built nandi.dll ----------
--- test.bat
+++ test.bat
@@ -4,14 +4,22 @@ call build.bat
 
 mkdir %OBJTEST% 2> NUL
 
-cl %C_FLAGS% %TEST%\main.test.c /Fo%OBJTEST%\main.test.obj
-cl %C_FLAGS% %TEST%\main.test.c /Fo%OBJTEST%\main.test.obj
-cl %C_FLAGS% %TEST%\n_graphics.test.c /Fo%OBJTEST%\n_graph.obj
-cl %C_FLAGS% %TEST%\n_math.test.c /Fo%OBJTEST%\n_math.test.obj
-cl %C_FLAGS% %TEST%\n_memory.test.c /Fo%OBJTEST%\n_memory.test.obj
+@echo on
+%CC% -c %C_FLAGS% %INCLUDES% %TEST%\main.test.c -o %OBJTEST%\main.test.o
+%CC% -c %C_FLAGS% %INCLUDES% %TEST%\n_graphics.test.c -o %OBJTEST%\n_graph.o
+%CC% -c %C_FLAGS% %INCLUDES% %TEST%\n_math.test.c -o %OBJTEST%\n_math.test.o
+%CC% -c %C_FLAGS% %INCLUDES% %TEST%\n_memory.test.c -o %OBJTEST%\n_memory.test.o
+@echo off
 
-link /NOLOGO %BUILD%\nandi.lib User32.lib Comctl32.lib %VULKAN_SDK%\Lib\vulkan-1.lib %OBJTEST%\*.obj ^
-    /OUT:%BUILD%\test.exe && ^
+setlocal enabledelayedexpansion
+set "ALL_OBJS="
+for %%f in (%OBJTEST%\*.o) do (
+    set "ALL_OBJS=!ALL_OBJS! %%f"
+)
+
+@echo on
+%CC% -o %BUILD%\test.exe %ALL_OBJS% %LIBS% -L%BUILD% -lnandi
+@echo off
 echo ---------- built test.exe ----------
 
 mkdir debug 2> NUL
